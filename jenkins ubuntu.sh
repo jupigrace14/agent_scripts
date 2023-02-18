f463d188164d4eb7197a31fc49bf68ac3feb7a65
JUPITER ACHE NFORBA <jupigrace14@gmail.com>
2023-02-18 13:04:05 -0500

    agent script

---
+++ jenkins ubuntu.sh
@@ -0,0 +1,17 @@
+!/bin/bash
+Author: Jupiter
+Description script to install Jenkins on ubuntu server
+Date: Feb 18 2023
+# this script is only tested on ubuntu xenial
+
+# install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+systemctl start docker
+usermod -aG docker ubuntu
